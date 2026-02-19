14643bf2c90fbc1b53ba4e61caf4e976d5456dc3
Yanela <yanelalzn05@gmail.com>
2026-02-19 02:07:00 -0500

    avance

--- js/main.js
+++ js/main.js
@@ -26,3 +26,54 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== Business ===============*/
+window.addEventListener("load", () => {
+
+    const scroll = document.getElementById("logoScroll");
+
+    // Duplicar contenido hasta que cubra mínimo 2 veces el ancho de pantalla
+    while (scroll.scrollWidth < window.innerWidth * 2) {
+        scroll.innerHTML += scroll.innerHTML;
+    }
+
+    let position = 0;
+    const speed = 0.5;
+
+    function animate() {
+        position -= speed;
+
+        if (Math.abs(position) >= scroll.scrollWidth / 2) {
+            position = 0;
+        }
+
+        scroll.style.transform = `translateX(${position}px)`;
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+});
+
+
+/*========== SERVICES SCROLL ==========*/
+const wrapper = document.getElementById("servicesWrapper");
+const section = document.querySelector(".services-section");
+
+window.addEventListener("scroll", () => {
+
+  const scrollTop = window.scrollY;
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  if (scrollTop >= sectionTop && scrollTop <= sectionTop + sectionHeight) {
+
+    // Progreso entre 0 y 1
+    let progress = (scrollTop - sectionTop) / (sectionHeight - window.innerHeight);
+
+    // Ajusta cuánto quieres que se mueva
+    let moveY = progress * (wrapper.scrollHeight - window.innerHeight);
+
+    wrapper.style.transform = `translateY(-${moveY}px)`;
+  }
+
+});
